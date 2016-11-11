b529cba73449e5623aff7efc87db3e72dd154694
Lzc6996 <lzc6996@foxmail.com>
2016-11-11 14:01:09 +0800

    Add files via upload

---
+++ test1.sh
@@ -0,0 +1,1 @@
+./paddle_trainer --config=trainer_config.conf1 --use_gpu=True --trainer_count=1 --log_period=200 --test_period=0 --num_passes=1 --save_dir=./ --job=test --test_pass=0
---
+++ test4.sh
@@ -0,0 +1,1 @@
+./paddle_trainer --config=trainer_config.conf4 --use_gpu=True --trainer_count=4 --log_period=200 --test_period=0 --num_passes=1 --save_dir=./ --job=test --test_pass=0
